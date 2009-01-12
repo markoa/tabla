fae9f6002b4edef9d048d9a9e3f86ae677a6c8bb
Marko Anastasov <marko.anastasov@gmail.com>
2009-01-12 01:28:59 +0100

    DRYing up js a bit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function authToken() {
+  return 'authenticity_token=' + encodeURIComponent(AUTH_TOKEN);
+}
+
+function remote_get_link_to(ajaxPath, linkId) {
+  Event.observe(linkId, 'click', function() {
+    var link = new Element(linkId);
+    new Ajax.Request(ajaxPath,
+      { asynchronous: true, evalScripts: true, method: 'get',
+        parameters: authToken()
+      });
+    return false;
+  });
+}
