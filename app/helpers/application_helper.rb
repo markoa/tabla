6cf4d0b59ee85bc02e31eee1f778371998742640
Marko Anastasov <marko.anastasov@gmail.com>
2009-01-14 11:33:03 +0100

    Handle external and links to nonexisting wiki pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  # Takes a number and returns a string like "23rd"
   def suffixed_number(num)
     last_digit = num.to_s.last
     case last_digit
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,20 +1,31 @@
 module PagesHelper
 
   def wikify(str)
-    out = auto_link str
+    out = str.clone
 
     # Wiki links
     # [wiki PageName]
+    # [smt else http://link.com]
     out.gsub!(/(!?)\[(.*?)(\|(.*?))?\]/) do |match|
-      name = $2.split.last
-      typed_name = name.clone
-      name.gsub!(/[A-Z]/) { |c| " #{c.downcase}" }.lstrip!
-      logger.info name
-      page = Page.find_by_name(name)
-      link_to typed_name, page_url(page)
+      tokens = $2.split
+      if tokens.size == 2 and tokens.first == "wiki"
+        a = tokens.last
+        page_name = a.clone
+        page_name.gsub!(/[A-Z]/) { |c| " #{c.downcase}" }.lstrip!
+
+        page = Page.find_by_name(page_name)
+        unless page.nil?
+          link_to(a, page_url(page))
+        else
+          link_to(a, new_page_url, :title => "Create this page")
+        end
+      else
+        a = tokens.slice(0, tokens.size-1).join(" ")
+        link_to(a, tokens.last)
+      end
     end
 
-    markdown out
+    markdown auto_link out
   end
 
 end
