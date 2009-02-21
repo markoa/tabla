21eec0c49850706d31d6e21dff0f0560e8d0df75
Marko Anastasov <marko.anastasov@gmail.com>
2009-02-21 23:31:35 +0100

    Removed unused revision scaffold code.

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -24,68 +24,6 @@ class RevisionsController < ApplicationController
     end
   end
 
-  # GET /revisions/new
-  # GET /revisions/new.xml
-  def new
-    @revision = Revision.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @revision }
-    end
-  end
-
-  # GET /revisions/1/edit
-  def edit
-    @revision = Revision.find(params[:id])
-  end
-
-  # POST /revisions
-  # POST /revisions.xml
-  def create
-    @revision = Revision.new(params[:revision])
-
-    respond_to do |format|
-      if @revision.save
-        flash[:notice] = 'Revision was successfully created.'
-        format.html { redirect_to(@revision) }
-        format.xml  { render :xml => @revision, :status => :created, :location => @revision }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @revision.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /revisions/1
-  # PUT /revisions/1.xml
-  def update
-    @revision = Revision.find(params[:id])
-
-    respond_to do |format|
-      if @revision.update_attributes(params[:revision])
-        flash[:notice] = 'Revision was successfully updated.'
-        format.html { redirect_to(@revision) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @revision.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /revisions/1
-  # DELETE /revisions/1.xml
-  def destroy
-    @revision = Revision.find(params[:id])
-    @revision.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(revisions_url) }
-      format.xml  { head :ok }
-    end
-  end
-
   protected
 
   def load_page
